5c0e6814ddad0effd61baf8b7f66eed79bdc83c0
Helw150 <wbh230@nyu.edu>
2025-04-10 19:45:39 -0400

    Counterfactual Run Script

---
+++ run_scripts/run_counterfactual_tasks.sh
@@ -0,0 +1,5 @@
+export CATS_CACHE_SEED=2
+
+python data/convert_from_hf.py --dataset WillHeld/EmoCF --split train --audio-dir EmoCF --output audio_inputs.jsonl
+python src/cats/inference.py --task emotion
+python src/cats/inference.py --task tone_aware_reply

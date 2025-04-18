8eeceb9c5ec43079014ef3f0aa8b85aeb480ca34
Helw150 <wbh230@nyu.edu>
2025-04-18 17:44:06 -0400

    Fix Jeopardy Errors

--- run_scripts/run_jeopardy.sh
+++ run_scripts/run_jeopardy.sh
@@ -1,5 +1,5 @@
 export CATS_CACHE_SEED=7
 
-#python data/convert_from_hf.py --dataset WillHeld/CartesiaJeopardy --split combined --audio-dir jeopardy --output audio_inputs.jsonl --exclude-columns filename
+python data/convert_from_hf.py --dataset WillHeld/CartesiaJeopardy --split combined --audio-dir jeopardy --output audio_inputs.jsonl --exclude-columns filename
 # Occasionally Futures Seem to Cause Hangs which matters a lot given the latency metrics
 python src/cats/inference.py --task jeopardy --workers 1

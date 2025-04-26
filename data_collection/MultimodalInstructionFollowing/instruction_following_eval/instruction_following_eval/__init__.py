bb251002068642b715323d4c69fcbd3ae1f0175c
imadtyx <adityashrivastava2799@gmail.com>
2025-04-26 06:29:15 +0000

    Vendor instruction_following_eval with local modifications

---
+++ data_collection/MultimodalInstructionFollowing/instruction_following_eval/instruction_following_eval/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["get_examples", "evaluate_instruction_following", "dict_to_input_example", "test_instruction_following"]
+
+from .evaluation import get_examples, evaluate_instruction_following, dict_to_input_example, test_instruction_following

3fb04fa254c1a8200a9e561a02583cf601b364c2
Helw150 <wbh230@nyu.edu>
2025-04-28 14:13:05 -0400

    Pip rather than submodule

--- data_collection/MultimodalInstructionFollowing/instruction_following_eval/instruction_following_eval/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["get_examples", "evaluate_instruction_following", "dict_to_input_example", "test_instruction_following"]
-
-from .evaluation import get_examples, evaluate_instruction_following, dict_to_input_example, test_instruction_following

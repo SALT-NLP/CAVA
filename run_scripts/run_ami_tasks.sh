45c6946938f550c1473d3154f451428aae3ecd4f
Michael Ryan <michael_ryan_2000@yahoo.com>
2025-05-02 08:28:16 -0700

    rename scripts from cats to cava (#31)

--- run_scripts/run_ami_tasks.sh
+++ run_scripts/run_ami_tasks.sh
@@ -2,5 +2,5 @@ export CATS_CACHE_SEED=5
 
 python data/convert_from_hf.py --dataset rma9248/CATS-ami-speaker-diarization-audio --split train --audio-dir SpeakerDiarization --output audio_inputs.jsonl
 python data/convert_from_hf.py --dataset rma9248/CATS-ami-next-speaker-audio --split train --audio-dir NextSpeaker --output audio_inputs.jsonl
-python src/cats/inference.py --task speaker_diarization
-python src/cats/inference.py --task next_speaker
+python src/cava/inference.py --task speaker_diarization
+python src/cava/inference.py --task next_speaker
